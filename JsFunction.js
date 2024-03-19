92f5cfff59a51dc116a6daeb2731e2966609e1c1
Ayibiowu Gbenga <ayibiowug@gmail.com>
2024-03-19 13:09:25 +0100

    Introduction to Js Functions

---
+++ JsFunction.js
@@ -0,0 +1,64 @@
+//  Assignment week 1
+
+// Question 1
+//  Write a function that returns square of a number
+
+// let findSquare = (x = "") => {
+//     let square = x ** 2
+//     console.log("The square of " + x + " is " + square);
+
+//     return square
+// }
+
+// findSquare (3)
+// findSquare (5)
+
+// Question 2
+//  Write a function that converts Celsius to Fahrenheit
+
+// let calFahrenheit = (celsius="") => {
+//    let fahrenheit = celsius * (9/5) + 32
+//     console.log(fahrenheit + " fahrenheit");
+//     return fahrenheit
+// }
+
+// calFahrenheit (2)
+// calFahrenheit (5)
+
+// Question 3
+//  Write a function that returns the Area and Perimeter of a circle
+
+// let circleValues = (radius="") => {
+// const pi = 22/7
+// let area= pi*radius**2
+// let Perimeter= 2*pi*radius
+
+// console.log("Area of circle : " +  area ,"Perimeter of circle : " + Perimeter);
+
+// return
+// }
+
+// circleValues (3)
+
+
+// Question 4
+// Write a function that calculates simple interest
+
+// simpleInt = (principal = "" , rate = "", time = "") => {
+// let percentageRate = rate/100;
+// let simpleInterest = principal * (1 + (percentageRate*time));
+
+// console.log("Final amount is " + simpleInterest);
+// return
+// }
+
+
+// Qustion 5
+// Write a function that calculates the area of a rectangle
+
+// 
+
+// Question 6
+// What are the two possible valves of the Boolean data type ?
+
+// True & false
